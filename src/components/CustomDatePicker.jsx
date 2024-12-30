cd4d0cbbda06197cf40d073b7916fbbf55f46f79
FuminocchiDesu <furuhashi0fumino@gmail.com>
2024-12-31 01:21:32 +0800

    Import CSS styles for CustomDatePicker component

--- src/components/CustomDatePicker.jsx
+++ src/components/CustomDatePicker.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useRef, useEffect } from 'react';
 import { LuCalendar } from "react-icons/lu";
+import 'assets/css/components';
 
 export default function CustomDatePicker ({ selectedDate, onChange }) {
     const [isOpen, setIsOpen] = useState(false);
